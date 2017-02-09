a5c553596ffeb9975bd90555d64a1afbd863a052
ghofer <gabor.hofer@frontsoft.hu>
2017-02-09 14:56:33 +0100

    webGL v.0.26

--- public/javascripts/c_drawtutorialwgl_5.js
+++ public/javascripts/c_drawtutorialwgl_5.js
@@ -2,19 +2,29 @@
 
 //user's view, control view, clipping, foreground/background control
 
-var DRAW_2D_SHAPE = true;
+/*
+ * Nézd át ezeket a turotial-okat, a mátrixokról szól!!!
+ * http://www.codinglabs.net/article_world_view_projection_matrix.aspx
+ * http://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/
+ * 
+ * 
+ */
+
+var DRAW_2D_SHAPE = false;
 
 var eyeX = 0.0;
 var eyeY = 0.0;
-var eyeZ = 1.0;
+var eyeZ = 5.0;
 var translateX = 0.0;
 var translateY = 0.0;
 var translateZ = 0.0;
 
-var step = 0.1;
+var step = 1.0;
 var px = 0.0;
 var py = 0.0;
 var pz = 0.0;
+
+var angle = 0.0;
 
 function wgl7_KeyDownHandler(event) {
     console.log('key code: ' + event.keyCode);
@@ -42,7 +52,12 @@ function wgl7_KeyDownHandler(event) {
     if (event.keyCode == 69) {	//e
         translateX += step;
     }
-    
+    if (event.keyCode == 65) {	//a
+        angle -= step;
+    }
+    if (event.keyCode == 68) {	//d
+        angle += step;
+    }
     wgl7_Draw();
 }
 
@@ -50,7 +65,7 @@ function wgl7_Draw() {
     
     var projMatrix = new Matrix4();
     //projMatrix.setOrtho(-2.0, 2.0, -2.0, 2.0, 10.0, -10.0);
-    projMatrix.setPerspective(120, webGLTutorialCanvas.width / webGLTutorialCanvas.height, 1, 100);
+    projMatrix.setPerspective(120, webGLTutorialCanvas.width / webGLTutorialCanvas.height, 1, 20);
     var u_ProjMatrix = gl.getUniformLocation(glProgram, 'u_ProjMatrix');
     gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
 
@@ -62,7 +77,7 @@ function wgl7_Draw() {
     
     var modelMatrix = new Matrix4();
     modelMatrix.setTranslate(translateX, translateY, translateZ);
-    //modelMatrix.setRotate(angle, 0, 0, 1);
+    modelMatrix.setRotate(angle, 0, 1, 0);
     var u_ModelMatrix = gl.getUniformLocation(glProgram, 'u_ModelMatrix');
     gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
         
@@ -74,8 +89,8 @@ function wgl7_Draw() {
         n = wgl7_InitVertexBuffers(gl);
         gl.drawArrays(gl.TRIANGLES, 0, n);
     } else {                                //kocka
-        n = wgl7_InitVertexBuffers_3D_Cube();
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+        n = wgl7_InitVertexBuffers_3D_Cube();        
+        gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);          //összefűzésben így kell rajzolni!!!
     }
     
 }
@@ -122,11 +137,48 @@ function wgl7_InitVertexBuffers() {
 }
 
 function wgl7_InitVertexBuffers_3D_Cube() {
-    var verticesTexCoords = new Float32Array([
-        
-    ]);
-    
+    var verticesColors = new Float32Array([
+        -1.0,  1.0,  1.0,   1.0, 0.0, 1.0,      /*magenta [0]*/
+         1.0,  1.0,  1.0,   1.0, 1.0, 1.0,      /*white   [1]*/
+         1.0, -1.0,  1.0,   1.0, 1.0, 0.0,      /*yellow  [2]*/
+        -1.0, -1.0,  1.0,   1.0, 0.0, 0.0,      /*red     [3]*/
 
+        -1.0,  1.0, -1.0,   0.0, 0.0, 1.0,      /*blue    [4]*/
+         1.0,  1.0, -1.0,   0.0, 1.0, 1.0,      /*cyan    [5]*/
+         1.0, -1.0, -1.0,   0.0, 1.0, 0.0,      /*green   [6]*/
+        -1.0, -1.0, -1.0,   0.0, 0.0, 0.0       /*black   [7]*/
+    ]);        
+    var vertexDefNumber = verticesColors.length / 6;            //ha összefűzés van, akkor nem ezzel térünk vissza
     
-    return vertexDefNumber;
+    var indices = new Uint8Array([
+        0, 1, 2,   0, 3, 2,                       /*front*/
+        0, 1, 4,   1, 4, 5,                       /*top*/
+        7, 6, 2,   7, 2, 3,                       /*bottom*/
+        4, 5, 6,   4, 7, 6,                       /*back*/
+        0, 7, 3,   0, 4, 7,                       /*left*/
+        1, 5, 2,   5, 6, 2                        /*right*/
+    ]);
+    var indicesLength = indices.length;                         //összefűzésnél az összefüző buffer hossza  a visszatérési érték
+    
+    //vertexBuffer
+    var vertexTexCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);    
+    
+    //indexbuffer (más a webGL rendszerben a cél: ELEMENT_ARRAY_BUFFER, erre figyelj!!! )
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    
+    var FSIZE = verticesColors.BYTES_PER_ELEMENT;
+    //vertices
+    var a_Position = gl.getAttribLocation(glProgram, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
+    gl.enableVertexAttribArray(a_Position);
+    //colors
+    var a_Color = gl.getAttribLocation(glProgram, 'a_Color');
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
+    gl.enableVertexAttribArray(a_Color);
+
+    return indicesLength;
 }
